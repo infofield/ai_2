96b10985457894756d43b7b04ff34a8215a84a1f
Krymov Aleksei <krymovalexey@gmail.com>
2024-05-28 17:03:27 +0400

    start

---
+++ ai_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ai_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] questions = { "привет", "как тебя зовут", "как дела" };
+            string[] answers = { "здравствуйте", "Абоба", "хорошо" };
+
+            string question = null;
+
+            do
+            {
+                question = Console.ReadLine();
+
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    if (question == questions[i])
+                    {
+                        Console.WriteLine(answers[i]);
+
+                        break;
+                    }
+                    else if (i == questions.Length - 1)
+                    {
+                        Console.WriteLine("Я еще маленький, объясни мне!");
+                    }
+                }
+            } while (true);
+
+        }
+    }
+}
